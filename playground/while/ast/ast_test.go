ea84fe15cbafea5bbbbb4d7b271840ca5de90bd2
at15 <at15@outlook.com>
2017-02-08 00:08:57 -0800

    [play][while] Add test for while command

--- playground/while/ast/ast_test.go
+++ playground/while/ast/ast_test.go
@@ -1,8 +1,8 @@
 package ast
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func assertArithExp(exp ArithExp, v int, t *testing.T) {
@@ -29,6 +29,24 @@ func assertBoolExp(exp BoolExp, b bool, t *testing.T) {
 	}
 }
 
+func assertCommandExp(exp CommandExp, postState State, t *testing.T) {
+	s := make(map[string]int)
+	exp.Eval(s)
+	// compare state
+	fail := false
+	for k, v := range postState {
+		if s[k] != v {
+			fmt.Printf("expect %s to be %d but got %d\n", k, v, s[k])
+			fail = true
+		}
+	}
+	if fail {
+		t.Fail()
+	} else {
+		fmt.Printf("%s => %v\n", exp, postState)
+	}
+}
+
 // TODO： test -， test *
 func TestArithSumExp_Eval(t *testing.T) {
 	var asst = func(exp ArithExp, v int) {
@@ -36,7 +54,6 @@ func TestArithSumExp_Eval(t *testing.T) {
 	}
 	asst(ArithSumExp{l: ArithValExp{num: 1}, r: ArithValExp{num: 2}}, 3)
 }
-
 
 // TODO: or, not, not eq, gt, les
 func TestBoolAndExp_Eval(t *testing.T) {
@@ -46,3 +63,18 @@ func TestBoolAndExp_Eval(t *testing.T) {
 	asst(BoolAndExp{l: BoolValExp{b: true}, r: BoolValExp{b: false}}, false)
 }
 
+// TODO: if, seq, assign Though, this test should cover most of them
+func TestCommandWhileExp_Eval(t *testing.T) {
+	var asst = func(exp CommandExp, s State) {
+		assertCommandExp(exp, s, t)
+	}
+
+	var exp = CommandSeqExp{c1: CommandAssignExp{v: VarExpr{name: "x"}, e: ArithValExp{num: 1}},
+		c2: CommandWhileExp{b: BoolLesExp{l: VarExpr{name: "x"}, r: ArithValExp{num: 100}},
+			c: CommandAssignExp{v: VarExpr{name: "x"}, e: ArithSumExp{l: VarExpr{name: "x"}, r: ArithValExp{num: 100}}}},
+	}
+
+	asst(exp, map[string]int{"x": 101})
+}
+
+// TODO: small step test
